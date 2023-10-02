eed06c41c632fabb492c93f2b391b3f1f910e541
admin <023>
2023-10-02 11:33:09 +0300

    zero test

--- _test.py
+++ _test.py
@@ -17,20 +17,25 @@ class Test:
     # -----------------------------------------------------------------------------------------------------------------
     # @pytest.mark.xfail
     def test__noClass(self):
+        count = 20
+        time_start = time.time()
+
         @ThreadsManager().decorator__thread_new
         def func(num):
             time.sleep(1)
             return num
 
-        for i in range(20):
+        for i in range(count):
             assert func(i) is None
 
         ThreadsManager.threads_wait_all()
 
-        assert len(ThreadsManager._threads) == 20
+        assert len(ThreadsManager._threads) == count
 
         for item in ThreadsManager._threads:
             assert item.is_alive() is False
+
+        assert time.time() - time_start < count/2
 
     def test__Class(self):
         pass
