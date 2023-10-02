b32c457eda6bc851ed38b8839d655ba19a1b2a88
admin <023>
2023-10-03 02:42:33 +0300

    add test__Class

--- _test.py
+++ _test.py
@@ -7,7 +7,7 @@ from tempfile import TemporaryDirectory
 from typing import *
 from configparser import ConfigParser
 
-from threads_manager import *
+from threading_manager import *
 
 
 # =====================================================================================================================
@@ -15,67 +15,33 @@ class Test:
     # VICTIM: Type[ThreadsManager] = type("VICTIM", (ThreadsManager,), {})
 
     # -----------------------------------------------------------------------------------------------------------------
-    # @pytest.mark.skip
-    # def test__noClass_oneManager(self):
-    #     # settings ------------------
-    #     count = 20
-    #     time_start = time.time()
-    #
-    #     # define victim ------------------
-    #     @ThreadsManager().decorator__to_thread
-    #     def func(num):
-    #         time.sleep(0.5)
-    #         return num
-    #
-    #     # spawn ------------------
-    #     for i in range(count):
-    #         assert func(i) is None
-    #
-    #     # wait ------------------
-    #     ThreadsManager().wait_all()
-    #
-    #     # checks ------------------
-    #     for item in ThreadsManager().THREAD_ITEMS:
-    #         assert item.is_alive() is False
-    #
-    #     assert time.time() - time_start < count/2
-    #
-    #     assert {item.result for item in ThreadsManager().THREAD_ITEMS} == {*range(count)}
-    #
-    #     # clear ------------------
-    #     assert len(ThreadsManager().THREAD_ITEMS) == count
-    #     assert ThreadsManager().COUNTER == count
-    #     ThreadsManager().thread_items__clear()
-    #     assert len(ThreadsManager().THREAD_ITEMS) == 0
-    #     assert ThreadsManager().COUNTER == 0
-
     def test__noClass_severalManagers(self):
         # settings ------------------
         count = 5
         time_start = time.time()
 
         # define victim ------------------
-        class Manager1(ThreadsManager):
+        class ThreadManager1(ThreadsManager):
             pass
 
-        class Manager2(ThreadsManager):
+        class ThreadManager2(ThreadsManager):
             pass
 
-        inst1 = Manager1()
-        inst2 = Manager2()
+        inst1 = ThreadManager1()
+        inst2 = ThreadManager2()
         assert id(inst1) != id(inst2)
-        assert Manager1() != Manager2()
-        assert Manager1() is not Manager2()
+        assert ThreadManager1() != ThreadManager2()
+        assert ThreadManager1() is not ThreadManager2()
 
-        assert Manager1().COUNTER == 0
-        assert Manager2().COUNTER == 0
+        assert ThreadManager1().COUNTER == 0
+        assert ThreadManager2().COUNTER == 0
 
-        @Manager1().decorator__to_thread
+        @ThreadManager1().decorator__to_thread
         def func1(num):
             time.sleep(1)
             return num * 10
 
-        @Manager2().decorator__to_thread
+        @ThreadManager2().decorator__to_thread
         def func2(num):
             time.sleep(1)
             return num * 100
@@ -84,31 +50,60 @@ class Test:
         for i in range(count):
             assert func1(i) is None
 
-        assert Manager1().COUNTER == count
-        assert Manager2().COUNTER == 0
+        assert ThreadManager1().COUNTER == count
+        assert ThreadManager2().COUNTER == 0
 
         for i in range(count):
             assert func2(i) is None
 
-        assert Manager1().COUNTER == count
-        assert Manager2().COUNTER == count
+        assert ThreadManager1().COUNTER == count
+        assert ThreadManager2().COUNTER == count
 
         # wait ------------------
-        Manager1().wait_all()
-        Manager2().wait_all()
+        ThreadManager1().wait_all()
+        ThreadManager2().wait_all()
 
         # checks ------------------
-        for item in Manager1().THREAD_ITEMS:
+        for item in ThreadManager1().THREAD_ITEMS:
             assert item.is_alive() is False
 
         assert time.time() - time_start < 5
 
-        assert {item.result for item in Manager1().THREAD_ITEMS} == {num * 10 for num in range(count)}
-        assert {item.result for item in Manager2().THREAD_ITEMS} == {num * 100 for num in range(count)}
+        assert {item.result for item in ThreadManager1().THREAD_ITEMS} == {num * 10 for num in range(count)}
+        assert {item.result for item in ThreadManager2().THREAD_ITEMS} == {num * 100 for num in range(count)}
 
-    def test__multy(self):
-        pass
+    def test__Class(self):
+        # settings ------------------
+        count = 5
+        time_start = time.time()
+
+        # define victim ------------------
+        class ThreadManager1(ThreadsManager):
+            pass
+
+        class Cls:
+            @ThreadManager1().decorator__to_thread
+            def func1(self, num):
+                time.sleep(1)
+                return num * 1000
+
+        # spawn ------------------
+        for i in range(count):
+            assert Cls().func1(i) is None
+
+        assert ThreadManager1().COUNTER == count
+        ThreadManager1().wait_all()
+        assert {item.result for item in ThreadManager1().THREAD_ITEMS} == {num * 1000 for num in range(count)}
+
+        ThreadManager1().thread_items__clear()
+
+        # spawn ------------------
+        for i in range(count):
+            assert Cls().func1(i) is None
+
+        assert ThreadManager1().COUNTER == count
+        ThreadManager1().wait_all()
+        assert {item.result for item in ThreadManager1().THREAD_ITEMS} == {num * 1000 for num in range(count)}
 
 
 # =====================================================================================================================
-
