302ec676622784c6f81f3be53a1d28a9d0bf429f
Andrei Starichenko <a.starichenko@element-t.ru>
2023-12-19 15:31:15 +0300

    ready v0.0.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as f:
 NAME = "threading_manager"
 
 setup(
-  version="0.0.4",
+  version="0.0.5",
   description="manage spawned threads",
   keywords=["threading", "threads", "thread", "manager"],
   classifiers=[
